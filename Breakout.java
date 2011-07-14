08a163ccfedc82dedefbdbf991270243021c4fd5
S <s>
2011-07-14 01:29:58 -0400

    {"type":"SWC", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -61,6 +61,7 @@ public class Breakout extends GraphicsProgram {
 /** Runs the Breakout program. */
 	public void run() {
 		addMouseListeners();
+		bgMusic.play();
 		setUpBricks();
 		setUpPaddle();
 		setUpBall();
