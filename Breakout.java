331775cac8d014c517174f0ce2af54283f58b349
S <s>
2011-07-13 23:57:30 -0400

    {"type":"SWC", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -166,7 +166,6 @@ public class Breakout extends GraphicsProgram {
 		}
 		return null; 
 	}
-	/* Private instance variables */
 	
 	private boolean ballNotAtBottom() {
 		if ((bally + BALL_RADIUS*2) > HEIGHT) {
@@ -178,8 +177,11 @@ public class Breakout extends GraphicsProgram {
 	private void endGame() {
 		GLabel end = new GLabel("GAME OVER");
 		end.setLocation((WIDTH - end.getWidth())/2.0, (HEIGHT - end.getAscent())/2.0);	
-	
 	}
+	
+	/* Private instance variables */
+
+	
 	GRect paddle = new GRect((WIDTH - PADDLE_WIDTH)/2.0, HEIGHT - (PADDLE_Y_OFFSET + PADDLE_HEIGHT), PADDLE_WIDTH, PADDLE_HEIGHT);
 	GOval ball = new GOval((WIDTH - BALL_RADIUS*2)/2.0, (HEIGHT - BALL_RADIUS*2)/2.0, BALL_RADIUS*2, BALL_RADIUS*2);
 	private RandomGenerator rgen = RandomGenerator.getInstance(); 
