9a0ed230fe7782fe050eac47c0cf488c4a34c16c
S <s>
2011-07-14 00:09:56 -0400

    {"type":"SWC", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -116,6 +116,7 @@ public class Breakout extends GraphicsProgram {
 	
 	private void setUpBall() {
 		ball.setLocation((WIDTH - BALL_RADIUS*2)/2.0, (HEIGHT - BALL_RADIUS*2)/2.0);
+		println("ball's y coor: " + bally);
 		ball.setFilled(true);
 		ball.setFillColor(Color.BLACK);
 		add(ball);
