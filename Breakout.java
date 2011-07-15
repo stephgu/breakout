bbad4c3bc79996ccb77138c4663aaaf095bf0726
S <s>
2011-07-14 20:36:32 -0400

    {"type":"SWC", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -243,7 +243,6 @@ public class Breakout extends GraphicsProgram {
 			win.setFont(new Font("Serif", Font.BOLD, 18));
 			win.setLocation((WIDTH - gameover.getWidth())/2.0, (HEIGHT - gameover.getAscent())/2.0);
 			add(win);
-			yourScore = 1000000;
 			end = win;
 		} else {
 			gameover.setFont(new Font("Serif", Font.BOLD, 18));
